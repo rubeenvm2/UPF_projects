c7cbc40cc763e54de90c165aee311ca171e7aaf3
rubeenvm2 <131793318+rubeenvm2@users.noreply.github.com>
2023-04-25 22:32:30 +0200

    Delete Parallel and distributed computing/lab3_TxGyy directory

--- Parallel and distributed computing/lab3_TxGyy/1_image/scripts/compare.sh
+++
@@ -1,4 +0,0 @@
-diff lleo-detect.txt  sol_ref/lleo-detect.txt
-diff lleo-enhance.txt sol_ref/lleo-enhance.txt
-diff lleo-inverse.txt sol_ref/lleo-inverse.txt
-diff lleo-smooth.txt  sol_ref/lleo-smooth.txt
